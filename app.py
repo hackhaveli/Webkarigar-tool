430c058121723e4f6ce941b022027e5721fd3572
Rohit hcr2 <159037919+hackhaveli@users.noreply.github.com>
2025-06-05 12:49:21 +0530

    update to run

--- app.py
+++ app.py
@@ -3034,5 +3034,5 @@ def email_sender_preview():
     }
     return jsonify(preview_data)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
